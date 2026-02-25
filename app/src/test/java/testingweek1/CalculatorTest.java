ee78c8ee20e78be2cc35d4ebde03435d20d73d90
AlpErenKaya0 <80575610+AlpErenKaya0@users.noreply.github.com>
2026-02-25 14:54:27 +0300

    TEST METHODS FİXED

--- app/src/test/java/testingweek1/CalculatorTest.java
+++ app/src/test/java/testingweek1/CalculatorTest.java
@@ -9,11 +9,11 @@ class CalculatorTest {
 
     @Test
     void testAddition() {
-        assertEquals(1, calculator.add(7, 3), "7 + 3 should equal 10");
+        assertEquals(10, calculator.add(7, 3), "7 + 3 should equal 10");
     }
 
     @Test
     void testSubtraction() {
-        assertEquals(1, calculator.subtract(10, 6), "10 - 6 should equal 4");
+        assertEquals(4, calculator.subtract(10, 6), "10 - 6 should equal 4");
     }
 }
